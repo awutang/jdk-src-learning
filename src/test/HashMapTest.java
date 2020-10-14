1d4c3a1eb492afd6805271e3def3481aefed85e1
awutang <779706234@qq.com>
2020-10-14 21:50:45 +0800

    first commit

---
+++ src/test/HashMapTest.java
@@ -0,0 +1,14 @@
+/**
+ * Author: Tang Yuqian
+ * Date: 2020/10/14
+ */
+package test;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap<>();
+        hashMap.put("xxx", 111);
+    }
+}
